2d65db9c90d62d6436a12f1498a4ac31526b6d18
Lena <lzhengem@gmail.com>
2018-10-09 17:22:43 -0700

    completed dictionariesandmaps

--- DictionariesAndMaps.java
+++ DictionariesAndMaps.java
@@ -47,22 +47,31 @@
 //Query 2: harry 
 //Harry is one of the keys in our dictionary, so we print harry=12299933.
 //Complete this code or write your own from scratch
+//Complete this code or write your own from scratch
 import java.util.*;
 import java.io.*;
 
-class DictionariesAndMaps{
+class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        Map<String,Integer> phonebook = new HashMap<String, Integer>();
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             // Write code here
+            phonebook.put(name, phone);
         }
         while(in.hasNext()){
             String s = in.next();
             // Write code here
+            if (phonebook.containsKey(s))
+                System.out.println(s + "=" +phonebook.get(s));
+            else 
+                System.out.println("Not found");
         }
         in.close();
+        //System.out.println(Arrays.asList(phonebook));
+        
     }
 }
